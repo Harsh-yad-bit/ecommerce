8d9ef9c2a59520598a801ce57394bfb38de3b793
Harsh <harshyadvv11@gmail.com>
2026-09-13 17:25:42 +0530

    Fix TextHome import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "@fontsource-variable/inter";
 import Header from '../components/Header';
 import Cursor from '../components/Cursor';
 import Maskimage from '../components/Maskimage';
-import Texthome from '../components/texthome';
+import Texthome from '../components/Texthome';
 import "../styles/homepage.css"
 
 const Home = () => {
